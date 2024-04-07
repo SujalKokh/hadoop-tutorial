e9eba922d6dd66b0179e9ed351e7ef89dd006931
Sujal Kokh Shrestha <sujal.kokh@gmail.com>
2024-04-07 23:43:08 +0545

    feat: hadoop mapper, reducer and job runnder class created

---
+++ src/main/java/org/sujalkokh/HadoopJobRunner.java
@@ -0,0 +1,29 @@
+package org.sujalkokh;
+
+import java.io.IOException;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.TextOutputFormat;
+
+public class HadoopJobRunner {
+    public static void main(String[] args) throws IOException{
+        JobConf conf = new JobConf(HadoopJobRunner.class);
+        conf.setJobName("WordCount");
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
+        conf.setMapperClass(HadoopMapper.class);
+        conf.setCombinerClass(HadoopReducer.class);
+        conf.setReducerClass(HadoopReducer.class);
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
+        FileInputFormat.setInputPaths(conf,new Path(args[0]));
+        FileOutputFormat.setOutputPath(conf,new Path(args[1]));
+        JobClient.runJob(conf);
+    }
+}
